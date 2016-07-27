fde557a685213cef380c2ad6243965e3fc3fea80
Chihab Otmani <chihab@gmail.com>
2016-07-27 12:32:32 +0100

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'ngtemplate?relativeTo=' + (path.resolve(__dirname)) + '/app/!html'
+            },
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['es2015'],
+                    cacheDirectory: true
+                }
+            }
+        ]
+    },
+    context: __dirname,
+    entry: "./app/index.js",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
+    }
+}
